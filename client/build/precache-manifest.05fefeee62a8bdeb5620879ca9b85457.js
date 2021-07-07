de6ea6ff1042f8a53b348335de2f74f966630b88
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-07-06 20:27:36 -0500

    update

---
+++ client/build/precache-manifest.05fefeee62a8bdeb5620879ca9b85457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45606d7167ce8ce4b5dc5764615c9a12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "639e6d3d152fda2fdc5f",
+    "url": "/static/css/main.2da496bc.chunk.css"
+  },
+  {
+    "revision": "aa3e15f5e8d220b05305",
+    "url": "/static/js/2.955ab476.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.955ab476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639e6d3d152fda2fdc5f",
+    "url": "/static/js/main.1edca196.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
