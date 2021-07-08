6418b27086aa1c993749a134d14d1f5c963d2d75
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-07-07 19:17:14 -0500

    updated landing page and responsive for mobile. iphone x and up

--- client/src/components/mainpages/landing/Page.js
+++ client/src/components/mainpages/landing/Page.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import './landing.css'
+import Button from '@material-ui/core/Button';
+
+
+
 
 function Page() {
+
+
+
+
+
     return (
-        <div>
-            Home
-            <button><Link to="/register">REGISTER</Link></button>
+        <div className="info">
+          <h1 className="title">Welcome to Shower Thoughts</h1> 
+          <h3>Where you can speak freely and ask any question you can think of without judgement. </h3>
+          <p>We've all had a "shower thought" or two. They're the fleeting thoughts you have while doing something mundane, like taking a shower or mowing the lawn. They're the miniature epiphanies that occur when your brain is occupied with doing something else, but suddenly you realize that you've come up with a totally unique way of thinking</p>
+          <div className="btn-c">
+            <Button color="primary" variant="contained" className="btn-c"><Link to="/register">REGISTER</Link></Button>
+          </div>  
         </div>
     );
 }
