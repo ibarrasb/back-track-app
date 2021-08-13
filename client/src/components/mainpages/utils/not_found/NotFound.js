cc25be2b482e1a8942107e5b89f7bbf00c2ba249
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-08-12 23:48:11 -0500

    updated 404 page

--- client/src/components/mainpages/utils/not_found/NotFound.js
+++ client/src/components/mainpages/utils/not_found/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Frown from '../../../../images/frown-regular.svg'
+import './notfound.css'
 
 function NotFound() {
     return (
         <div>
+        <img src={Frown} alt="Frown" className="frown"/>
+        <div  className="not-found">
+        
             404 | Not Found
+        </div>
         </div>
     )
 }
