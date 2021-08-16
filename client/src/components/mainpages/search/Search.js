e289812f9a982540a88957ce4e460c7757e9f274
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-08-16 12:23:40 -0500

    ui added to search

--- client/src/components/mainpages/search/Search.js
+++ client/src/components/mainpages/search/Search.js
@@ -3,11 +3,15 @@ import {GlobalState} from '../../../GlobalState'
 import { TextField } from '@material-ui/core';
 
 function Search() {
+    //searches for user profiles 
+
     const state = useContext(GlobalState)
     const [users] = state.AllUsersAPI.users
+
     return (
         <div>
-        <TextField id="outlined-basic" label='Search by username' variant="outlined" type="text" name="post" className="in-post" value ={post.post} onChange={handleChangeInput} >
+        
+        <TextField id="outlined-basic" label='Search by username' variant="outlined" type="text" name="post" className="in-post" >
         </TextField>
             
         </div>
